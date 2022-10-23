591e337448369a0a0316f16b64aa6836596ca3f4
Nasief Khan <nasiefk14@gmail.com>
2022-10-24 00:43:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDV-ZM48gYCtDeZQkuiw2YjA-wcVU21ltY",
+    authDomain: "fir-tutorial-8d4c2.firebaseapp.com",
+    projectId: "fir-tutorial-8d4c2",
+    storageBucket: "fir-tutorial-8d4c2.appspot.com",
+    messagingSenderId: "962217852787",
+    appId: "1:962217852787:web:c6370cbe624bf85d7f0f06",
+    measurementId: "G-2ERSXKYLVV"
+};
+
+initializeApp(firebaseConfig)
